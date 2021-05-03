dd1f783992280d6471b56c58a2bf9ca44b519592
Valentin Popov <VOPopov@luxoft.com>
2021-05-03 09:42:41 +0300

    initial commit

---
+++ src/main/java/ru/valeo/jim/JimApplication.java
@@ -0,0 +1,13 @@
+package ru.valeo.jim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JimApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/valeo/jim/JimApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.valeo.jim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
