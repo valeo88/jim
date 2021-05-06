a9389e1f807aeeb63af1adef39e7a448d7f6b6e9
Valentin Popov <VOPopov@luxoft.com>
2021-05-06 09:41:41 +0300

    + DictionariesServiceTest

---
+++ src/test/java/ru/valeo/jim/service/DictionariesServiceTest.java
@@ -0,0 +1,88 @@
+package ru.valeo.jim.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+class DictionariesServiceTest {
+
+    @Autowired
+    private DictionariesService service;
+
+    @Test
+    void shouldHaveSomeCurrencies() {
+        assertFalse(service.getCurrencies().isEmpty());
+    }
+
+    @Test
+    void shouldSaveNewCurrency() {
+        var newCurrency = service.saveCurrency("XXX", "New currency", "000");
+
+        var currencies = service.getCurrencies();
+        assertTrue(currencies.contains(newCurrency));
+    }
+
+    @Test
+    void shouldUpdateExistingCurrency() {
+        var currencyCode = service.getCurrencies().get(0).getCode();
+
+        var updated = service.saveCurrency(currencyCode, "New currency", "000");
+
+        var currencies = service.getCurrencies();
+        assertTrue(currencies.contains(updated));
+    }
+
+    @Test
+    void shouldDeleteCurrencyIfExists() {
+        String code = "XXX";
+        var newCurrency = service.saveCurrency("XXX", "New currency", "000");
+
+        service.deleteCurrency(code);
+
+        var currencies = service.getCurrencies();
+        assertFalse(currencies.contains(newCurrency));
+    }
+
+    @Test
+    void shouldHaveSomeInstrumentCategories() {
+        assertFalse(service.getInstrumentCategories().isEmpty());
+    }
+
+    @Test
+    void shouldSaveNewInstrumentCategory() {
+        var newCategory = service.saveInstrumentCategory("XXX", "New category");
+
+        var categories = service.getInstrumentCategories();
+        assertTrue(categories.contains(newCategory));
+    }
+
+    @Test
+    void shouldUpdateExistingInstrumentCategory() {
+        var code = service.getInstrumentCategories().get(0).getCode();
+
+        var updated = service.saveInstrumentCategory(code, "New category");
+
+        var categories = service.getInstrumentCategories();
+        assertTrue(categories.contains(updated));
+    }
+
+    @Test
+    void shouldDeleteInstrumentCategoryIfExists() {
+        String code = "XXX";
+        var newCategory = service.saveInstrumentCategory("XXX", "New currency");
+
+        service.deleteInstrumentCategory(code);
+
+        var categories = service.getInstrumentCategories();
+        assertFalse(categories.contains(newCategory));
+    }
+
+    @Test
+    void getInstrumentTypes() {
+        assertFalse(service.getInstrumentTypes().isEmpty());
+    }
+}
